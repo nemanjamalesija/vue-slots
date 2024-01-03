05461604c5e25084ee826dcaec8a2a66b9dbdd5e
Nemanja <malesija.nemanja@gmail.com>
2024-01-03 11:53:58 +0100

    Introduce middle component

---
+++ src/components/MiddleComponent.vue
@@ -0,0 +1,16 @@
+<script>
+import WelcomeItem from './WelcomeItem.vue'
+
+export default {
+  components: {
+    WelcomeItem
+  }
+}
+</script>
+<template>
+  <WelcomeItem>
+    <template v-for="(_, slot) in $slots" v-slot:[slot]="props">
+      <slot :name="slot" v-bind="props" />
+    </template>
+  </WelcomeItem>
+</template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,14 +1,20 @@
-<script setup>
-import WelcomeItem from './WelcomeItem.vue'
+<script>
+import MiddleComponent from './MiddleComponent.vue'
+
+export default {
+  components: {
+    MiddleComponent
+  }
+}
 </script>
 
 <template>
-  <WelcomeItem api-url="url" :per-page="10">
+  <MiddleComponent>
     <template v-slot:item="{ body, username, likes }">
       <div class="item">
         <p>{{ body }}</p>
         <p class="meta">by {{ username }} | {{ likes }} likes</p>
       </div>
     </template>
-  </WelcomeItem>
+  </MiddleComponent>
 </template>
