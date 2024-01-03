e0e43d57cd3a654735fbf1c63a94346369aa3e06
Nemanja <malesija.nemanja@gmail.com>
2024-01-03 11:17:40 +0100

    Set scoped slots, test with destructuring in a loop

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -1,86 +1,41 @@
+<script>
+export default {
+  props: ['api-url', 'per-page'],
+  data() {
+    return {
+      items: []
+    }
+  },
+  mounted() {
+    // mock remote data fetching
+    setTimeout(() => {
+      this.items = [
+        { body: 'Scoped Slots Guide', username: 'Evan You', likes: 20 },
+        { body: 'Vue Tutorial', username: 'Natalia Tepluhina', likes: 10 }
+      ]
+    }, 1000)
+  }
+}
+</script>
+
 <template>
-  <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
-    <div class="details">
-      <h3>
-        <slot name="heading"></slot>
-      </h3>
-      <slot></slot>
-    </div>
-  </div>
+  <ul>
+    <li v-if="!items.length">Loading...</li>
+    <li v-for="{ body, username, likes } in items">
+      <slot name="item" v-bind="{ body, username, likes }" />
+    </li>
+  </ul>
 </template>
 
 <style scoped>
-.item {
-  margin-top: 2rem;
-  display: flex;
-  position: relative;
+ul {
+  list-style-type: none;
+  padding: 5px;
+  background: linear-gradient(315deg, #42d392 25%, #647eff);
 }
-
-.details {
-  flex: 1;
-  margin-left: 1rem;
-}
-
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-  color: var(--color-text);
-}
-
-h3 {
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
-}
-
-@media (min-width: 1024px) {
-  .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .item:before {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
-  }
+li {
+  padding: 5px 20px;
+  margin: 10px;
+  background: #fff;
 }
 </style>
